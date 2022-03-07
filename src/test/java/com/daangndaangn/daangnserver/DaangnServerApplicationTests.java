a86c50da58cfc2c0d02690131c5691957a2d206b
zaehuun <jdrjk0000@naver.com>
2022-03-07 23:19:06 +0900

    Init: 프로젝트 초기 환경 설정
    Spring Web, MySQL Driver, JPA 등 의존성을 추가했습니다.

---
+++ src/test/java/com/daangndaangn/daangnserver/DaangnServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daangndaangn.daangnserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
