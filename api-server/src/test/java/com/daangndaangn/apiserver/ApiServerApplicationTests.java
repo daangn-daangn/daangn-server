1e09f633265920385979af187b4cfa7b8322a971
zhtcks <zhtcks@gmail.com>
2022-03-15 04:44:46 +0900

    Init: gradle 멀티 모듈 설정

--- api-server/src/test/java/com/daangndaangn/apiserver/ApiServerApplicationTests.java
+++ api-server/src/test/java/com/daangndaangn/apiserver/ApiServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.daangndaangn.daangnserver;
+package com.daangndaangn.apiserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DaangnServerApplicationTests {
+class ApiServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
