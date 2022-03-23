bbb833092cc21fa62552928ca7a37c7151b1bded
zaehuun <jdrjk0000@naver.com>
2022-03-24 06:04:18 +0900

    Feat: #4 - 채팅 방 모델 ID 추가
    ID로 접근이 가능하도록 ID를 추가했습니다.

--- chat-server/src/main/java/com/daangndaangn/chatserver/ChattingRoom/model/ChattingRoom.java
+++ chat-server/src/main/java/com/daangndaangn/chatserver/ChattingRoom/model/ChattingRoom.java
@@ -1,11 +1,15 @@
 package com.daangndaangn.chatserver.ChattingRoom.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.HashMap;
 
 @Document(collation = "chatting-room")
 public class ChattingRoom {
+
+    @Id
+    private String id;
 
     private String nickname;
 
