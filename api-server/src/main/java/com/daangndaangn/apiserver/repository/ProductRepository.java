398dbd3845b2c544fcc28816fd2d09213c77cce4
zaehuun <jdrjk0000@naver.com>
2022-04-04 03:15:39 +0900

    Feat: #11 - 물품 관련 CRUD 메소드 Service 내 정의
    물품과 관련된 메소드들을 Service 내 추가했습니다.

--- api-server/src/main/java/com/daangndaangn/apiserver/repository/ProductRepository.java
+++ api-server/src/main/java/com/daangndaangn/apiserver/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.daangndaangn.apiserver.repository;
 import com.daangndaangn.apiserver.entity.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findById(Long productId);
 }
--- api-server/src/main/java/com/daangndaangn/apiserver/service/product/ProductServiceImpl.java
+++ api-server/src/main/java/com/daangndaangn/apiserver/service/product/ProductServiceImpl.java
@@ -1,6 +1,12 @@
 package com.daangndaangn.apiserver.service.product;
 
+import com.daangndaangn.apiserver.controller.product.ProductResponse;
+import com.daangndaangn.apiserver.entity.category.Category;
+import com.daangndaangn.apiserver.entity.product.Product;
+import com.daangndaangn.apiserver.entity.product.ProductState;
+import com.daangndaangn.apiserver.error.NotFoundException;
 import com.daangndaangn.apiserver.repository.ProductRepository;
+import com.daangndaangn.apiserver.service.category.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,9 +18,56 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService{
 
     private final ProductRepository productRepository;
+    private final CategoryService categoryService;
 
     @Override
-    public void createProduct(String title, Long category, Long price, String content, List<String> imgUrl) {
+    public ProductResponse.GetResponse getProduct(Long productId){
+        Product product = productRepository.findById(productId).orElseThrow(() -> new NotFoundException(Product.class, String.format("productId = %s", productId)));
+        return convertToDto(product);
+    }
+    @Override
+    @Transactional
+    public ProductResponse.CreateResponse createProduct(String title, String name, Long categoryId, Long price, String description, List<String> imgUrlList) {
+        Category category = categoryService.getCategory(categoryId);
+        Product product = Product.builder()
+                .title(title)
+                .name(name)
+                .category(category)
+                .price(price)
+                .state(ProductState.FOR_SALE)
+                .description(description)
+                .seller(null)
+                .buyer(null)
+                .imgUrlList(imgUrlList)
+                .build();
+        return ProductResponse.CreateResponse.of(productRepository.save(product).getId());
+    }
 
+    @Override
+    @Transactional
+    public ProductResponse.UpdateResponse updateProduct(Long productId, String title, String name, Long categoryId, Long price, String description, List<String> imgUrlList){
+        Product product = productRepository.findById(productId).orElseThrow(() -> new NotFoundException(Product.class, String.format("productId = %s", productId)));
+        Category category = categoryService.getCategory(categoryId);
+        product.setTitle(title);
+        product.setName(name);
+        product.setCategory(category);
+        product.setPrice(price);
+        product.setDescription(description);
+        product.setProductImageList(imgUrlList);
+        product.setThumbNailImage(imgUrlList);
+
+        return ProductResponse.UpdateResponse.of(productRepository.save(product).getId());
+    }
+
+    @Override
+    @Transactional
+    public ProductResponse.DeleteResponse deleteProduct(Long productId) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new NotFoundException(Product.class, String.format("productId = %s", productId)));
+        productRepository.delete(product);
+        return ProductResponse.DeleteResponse.of(true);
+    }
+
+    private ProductResponse.GetResponse convertToDto(Product product) {
+        return ProductResponse.GetResponse.from(product);
     }
 }
