f8610719622d6440d8576924703ab89e85b14093
zaehuun <jdrjk0000@naver.com>
2022-04-04 03:11:03 +0900

    Feat: #11 - ProductService 인터페이스 정의
    Product에 대한 메소드를 정의했습니다.

--- api-server/src/main/java/com/daangndaangn/apiserver/service/product/ProductService.java
+++ api-server/src/main/java/com/daangndaangn/apiserver/service/product/ProductService.java
@@ -1,10 +1,15 @@
 package com.daangndaangn.apiserver.service.product;
 
+import com.daangndaangn.apiserver.controller.product.ProductResponse;
+
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public interface ProductService {
+    ProductResponse.GetResponse getProduct(Long productId);
+    ProductResponse.CreateResponse createProduct(String title, String name, Long categoryId, Long price, String description, List<String> imgUrlList);
+    ProductResponse.UpdateResponse updateProduct(Long productId, String title, String name, Long categoryId, Long price, String description, List<String> imgUrlList);
+    ProductResponse.DeleteResponse deleteProduct(Long productId);
 
-    void createProduct(String title, Long category, Long price, String content, List<String> imgUrl);
 
 }
