8285712bf3c760c29a9631f96b2460458d571461
zaehuun <jdrjk0000@naver.com>
2022-04-03 16:18:43 +0900

    Feat: #11 - 물품 Service에 물품 생성 메소드 추가
    물품을 생성하는 createProduct 메소드를 추가했습니다.

--- api-server/src/main/java/com/daangndaangn/apiserver/service/product/ProductService.java
+++ api-server/src/main/java/com/daangndaangn/apiserver/service/product/ProductService.java
@@ -1,4 +1,10 @@
 package com.daangndaangn.apiserver.service.product;
 
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 public interface ProductService {
+
+    void createProduct(String title, Long category, Long price, String content, List<String> imgUrl);
+
 }
--- api-server/src/main/java/com/daangndaangn/apiserver/service/product/ProductServiceImpl.java
+++ api-server/src/main/java/com/daangndaangn/apiserver/service/product/ProductServiceImpl.java
@@ -5,9 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService{
 
     private final ProductRepository productRepository;
+
+    @Override
+    public void createProduct(String title, Long category, Long price, String content, List<String> imgUrl) {
+
+    }
 }
