7af2ffb762085dd73fe570a62f0464de58e23a11
zaehuun <jdrjk0000@naver.com>
2022-04-23 04:19:59 +0900

    Feat: #17 - Product QueryDSL 함수 구현
    위치 기반 검색과 필터 기반 검색을 위한 Query DSL 함수를 구현했습니다.

---
+++ api-server/src/main/java/com/daangndaangn/apiserver/repository/product/ProductRepositoryImpl.java
@@ -0,0 +1,75 @@
+package com.daangndaangn.apiserver.repository.product;
+
+import com.daangndaangn.apiserver.entity.product.Product;
+import com.daangndaangn.apiserver.entity.product.ProductState;
+import com.daangndaangn.apiserver.entity.product.QProduct;
+import com.daangndaangn.apiserver.entity.user.Location;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductCustom{
+
+    private static final QProduct qProduct = QProduct.product;
+    private final JPAQueryFactory jpaQueryFactory;
+    @Override
+    public List<Product> findAllProduct(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Product> findAllProductByLocation(Location location, Pageable pageable) {
+
+        return jpaQueryFactory.select(qProduct)
+                .from(qProduct)
+                .join(qProduct.category).fetchJoin()
+                .where(
+                        qProduct.location.eq(location)
+                                .and(qProduct.state.eq(ProductState.FOR_SALE)))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    @Override
+    public List<Product> findAllProductByFilter(String keyword, Long minPrice, Long maxPrice, Long categoryId, Location location, Pageable pageable) {
+        System.out.println(789);
+        return jpaQueryFactory.select(qProduct)
+                .from(qProduct)
+                .join(qProduct.category).fetchJoin()
+                .where(
+                        this.eqKeyword(keyword),
+                        this.eqCategory(categoryId),
+                        this.rangePrice(minPrice, maxPrice),
+                        qProduct.location.eq(location)
+                        .and(qProduct.state.eq(ProductState.FOR_SALE)))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private BooleanExpression eqKeyword(String keyword){
+        if(StringUtils.isEmpty(keyword)){
+            return null;
+        }
+        return qProduct.title.contains(keyword);
+    }
+
+    private BooleanExpression rangePrice(Long minPrice, Long maxPrice){
+        if(minPrice == null && maxPrice == null) return null;
+        if(minPrice != null && maxPrice == null) return qProduct.price.goe(minPrice);
+        return qProduct.price.between(minPrice, maxPrice);
+    }
+
+    private BooleanExpression eqCategory(Long categoryId){
+        if(categoryId == null){
+            return null;
+        }
+        return qProduct.category.id.eq(categoryId);
+    }
+}
