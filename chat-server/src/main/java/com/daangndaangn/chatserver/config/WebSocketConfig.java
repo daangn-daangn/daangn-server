8d477d0f493b05cbe45208a0afe04d34c660890f
zhtcks <75410527+cotchan@users.noreply.github.com>
2022-07-07 03:25:53 +0900

    delete: chat-server kafka 제거

--- chat-server/src/main/java/com/daangndaangn/chatserver/config/WebSocketConfig.java
+++ chat-server/src/main/java/com/daangndaangn/chatserver/config/WebSocketConfig.java
@@ -10,6 +10,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /**
+     * SockJs Fallback을 이용해 노출할 STOMP endpoint를 설정하는 부분입니다.
+     * sockJs 클라이언트가 Websocket 핸드셰이크를 하기 위해 연결할 endpoint를 지정할 수 있습니다.
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
@@ -18,13 +22,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     /**
-     * .setApplicationDestinationPrefixes
-     *      client에서 SEND 요청을 처리합니다.
-     *      클라이언트가 발행하는 요청 (publish)을 처리하는 prefix 설정
-     *
      * .enableSimpleBroker
-     *      해당 경로로 SimpleBroker를 등록합니다.
-     *      SimpleBroker는 해당하는 경로를 SUBSCRIBE하는 client에게 메시지를 전달하는 간단한 작업을 수행
+     *     MessageBroker에 연결할 prefix를 적습니다.
+     *     해당 설정을 적용하면, enableSimpleBroker에 추가된 엔드포인트 prefix를 달고 들어오는 요청은 브로커 채널에 전달됩니다.
      */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
--- chat-server/src/main/java/com/daangndaangn/chatserver/controller/MessageSender.java
+++ chat-server/src/main/java/com/daangndaangn/chatserver/controller/MessageSender.java
@@ -3,8 +3,7 @@ package com.daangndaangn.chatserver.controller;
 import com.daangndaangn.chatserver.controller.message.ChatMessageRequest.CreateRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 
@@ -13,13 +12,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MessageSender {
 
-    @Value("${spring.kafka.topic.chat-message}")
-    private String kafkaTopic;
-
-    private final KafkaTemplate<String, CreateRequest> kafkaTemplate;
+    private final SimpMessagingTemplate template;
 
     public void send(CreateRequest message) {
         //Sending the message to kafka topic queue
-        kafkaTemplate.send(kafkaTopic, message);
+        template.convertAndSend("/topic/chat/" + message.getRoomId(), message);
     }
 }
