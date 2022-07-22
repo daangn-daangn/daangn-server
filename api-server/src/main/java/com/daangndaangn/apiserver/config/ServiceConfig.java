821bafae15e13ea83cc779c51a58e0b8bcd564a4
zhtcks <75410527+cotchan@users.noreply.github.com>
2022-07-22 22:42:45 +0900

    update: jwt expirySecond 수정

--- api-server/src/main/java/com/daangndaangn/apiserver/config/ServiceConfig.java
+++ api-server/src/main/java/com/daangndaangn/apiserver/config/ServiceConfig.java
@@ -3,6 +3,7 @@ package com.daangndaangn.apiserver.config;
 import com.daangndaangn.common.config.JwtConfig;
 import com.daangndaangn.common.jwt.Jwt;
 import com.daangndaangn.common.util.MessageUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 /**
  * 프로젝트에서 사용하는 각종 Bean 모음
  */
+@Slf4j
 @Configuration
 public class ServiceConfig {
 
@@ -31,6 +33,7 @@ public class ServiceConfig {
 
     @Bean
     public Jwt jwt(JwtConfig jwtConfig) {
+        log.warn("jwt expirySeconds: {}",jwtConfig.getExpirySeconds());
         return new Jwt(jwtConfig.getIssuer(), jwtConfig.getClientSecret(), jwtConfig.getExpirySeconds());
     }
 
